9898e263f2e53e6a3439eb1587d824d2d92fb622
18768442094 <liyimin_6838@126.com>
2020-03-11 18:21:24 +0800

    创建名片成功返回名片id

--- src/main/java/com/lym/mechanical/CarstoreApplication.java
+++ src/main/java/com/lym/mechanical/CarstoreApplication.java
@@ -1,6 +1,7 @@
 package com.lym.mechanical;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
@@ -39,11 +40,11 @@ public class CarstoreApplication {
                 context.addConstraint(constraint);
             }
         };
-        //tomcat.addAdditionalTomcatConnectors(httpConnector());
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
         return tomcat;
     }
 
-    /*@Bean
+    @Bean
     public Connector httpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
@@ -51,7 +52,7 @@ public class CarstoreApplication {
         connector.setPort(7004);
         connector.setSecure(false);
         //监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(8081);
         return connector;
-    }*/
+    }
 }
